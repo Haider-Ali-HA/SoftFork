60298db813140b4e3b9e9cae74e47941384b9811
Mehdi Ali <mehdikhanofficial@gmail.com>
2024-11-30 12:22:49 +0500

    font and colors addd

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#772787", //purple
+        secondary: "#64607D",// dark gray
+        light: "#8D8D8D", //gray
+      },
       fontFamily: {
         gilroy: ["Gilroy", "sans-serif"],
         manrope: ["Manrope", "sans-serif"],
+        DMsans: ["DM Sans", "sans-serif"],
       },
     },
   },
